c355618cf5a216a82ea2a4a96a21548acfebe808
bwlewis <blewis@illposed.net>
2015-10-29 21:44:35 -0400

    Misc. cleanup, tests basic first draft is ready

--- tests/inmemory.R
+++ tests/inmemory.R
@@ -1,6 +1,22 @@
 library(lz4)
 
+# Test basic compression and decompression
+data(airquality)
 x <- airquality
 y <- lzCompress(serialize(x,NULL))
-z <- deserialize(lzDecompress(y))
-if(!isTRUE(all.equzl(x,z))) stop("decompressed object does not match original")
+z <- unserialize(lzDecompress(y))
+if(!isTRUE(all.equal(x,z))) stop("decompressed serialized R object does not match original")
+
+# Test character conversion
+data(iris)
+x <- capture.output(iris)
+y <- lzCompress(x)
+z <- readLines(textConnection(rawToChar(lzDecompress(y))))
+if(!isTRUE(all.equal(x,z))) stop("decompressed text does not match original")
+
+# Other compression modes
+data(EuStockMarkets)
+eu <- serialize(EuStockMarkets, NULL)
+ans <- lapply(seq(0,9), function(j)
+    object.size(lzCompress(eu, j))
+  )
